d14cae38d32d494664f1d40cf440f52c7812f0e2
LateForTrain <latefortrain@gmail.com>
2024-05-10 22:02:30 +0200

    Gui update
    Gui update

---
+++ MyTelegramBotGUI.py
@@ -0,0 +1,193 @@
+#! /usr/bin/env python3
+#  -*- coding: utf-8 -*-
+#
+# GUI definition for MyTelegramBot
+# version started 2024-05-10
+
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import scrolledtext
+from tkinter.constants import *
+import MyTelegramBotGUI_support
+
+#This class configures and populates the toplevel window.
+#top is the toplevel containing window
+class Toplevel1:
+     def __init__(self, top=None):
+          #checkbox var
+          self.WeatherOn_var=tk.BooleanVar()
+          self.WeatherOn_var=tk.BooleanVar()
+          self.HolidaysOn_var=tk.BooleanVar()
+          self.DateTimeOn_var=tk.BooleanVar()
+          self.AircraftOn_var=tk.BooleanVar()
+          self.DiceOn_var=tk.BooleanVar()
+          self.WebcamOn_var=tk.BooleanVar()
+          self.DefineOn_var=tk.BooleanVar()
+          self.TrafikOn_var=tk.BooleanVar()
+          self.PolisenOn_var=tk.BooleanVar()
+          self.MapOn_var=tk.BooleanVar()
+
+          #frame information
+          app_width=550
+          app_height=350
+          top.title("MyTelegramBot - Telegram Bot Control")
+          top.geometry(str(app_width)+"x"+str(app_height))
+          top.resizable(0,  0)
+
+          # Menu setup
+          menubar = tk.Menu(top)
+          # Adding Main Menu 
+          main = tk.Menu(menubar, tearoff = 0) 
+          menubar.add_cascade(label ='File', menu = main) 
+          main.add_command(label ='Clear Messages', command = lambda:MyTelegramBotGUI_support.clear_messages()) 
+          main.add_command(label ='Exit', command = lambda:MyTelegramBotGUI_support.close_app())
+
+          # Adding Help Menu 
+          help_ = tk.Menu(menubar, tearoff = 0) 
+          menubar.add_cascade(label ='Help', menu = help_) 
+          help_.add_command(label ='Tk Help', command = None) 
+          help_.add_separator() 
+          help_.add_command(label ='About TBC', command = lambda:MyTelegramBotGUI_support.about_msg())
+
+          top.config(menu = menubar) 
+
+          #items in the frame
+          self.top = top
+
+          self.Labelframe1 = tk.LabelFrame(self.top)
+          self.Labelframe1.place(x=5, y=5, height=100, width=430)
+          self.Labelframe1.configure(relief='groove')
+          self.Labelframe1.configure(text="Select Function")
+
+          self.WeatherOn = tk.Checkbutton(self.Labelframe1)
+          self.WeatherOn.place(x=0, y=10, height=20, width=100)
+          self.WeatherOn.configure(text="Weather")
+          self.WeatherOn.configure(anchor="w")
+          self.WeatherOn.configure(offvalue=False)
+          self.WeatherOn.configure(onvalue=True)
+          self.WeatherOn.configure(variable=self.WeatherOn_var)
+          self.WeatherOn.configure(command=lambda:MyTelegramBotGUI_support.Check_Press("WeatherOn"))
+          
+          self.HolidaysOn = tk.Checkbutton(self.Labelframe1)
+          self.HolidaysOn.place(x=0, y=30, height=20, width=100)
+          self.HolidaysOn.configure(text="Holidays")
+          self.HolidaysOn.configure(anchor="w")
+          self.HolidaysOn.configure(offvalue=False)
+          self.HolidaysOn.configure(onvalue=True)
+          self.HolidaysOn.configure(variable=self.HolidaysOn_var)
+          self.HolidaysOn.configure(command=lambda:MyTelegramBotGUI_support.Check_Press("HolidaysOn"))
+
+          self.TrafikOn = tk.Checkbutton(self.Labelframe1)
+          self.TrafikOn.place(x=0, y=50, height=20, width=100)
+          self.TrafikOn.configure(text="Traffic")
+          self.TrafikOn.configure(anchor="w")
+          self.TrafikOn.configure(offvalue=False)
+          self.TrafikOn.configure(onvalue=True)
+          self.TrafikOn.configure(variable=self.TrafikOn_var)
+          self.TrafikOn.configure(command=lambda:MyTelegramBotGUI_support.Check_Press("TrafficOn"))
+
+          self.DateTimeOn = tk.Checkbutton(self.Labelframe1)
+          self.DateTimeOn.place(x=100, y=10, height=20, width=100)
+          self.DateTimeOn.configure(text="Date/Time")
+          self.DateTimeOn.configure(anchor="w")
+          self.DateTimeOn.configure(offvalue=False)
+          self.DateTimeOn.configure(onvalue=True)
+          self.DateTimeOn.configure(variable=self.DateTimeOn_var)
+          self.DateTimeOn.configure(command=lambda:MyTelegramBotGUI_support.Check_Press("DateTimeOn"))
+
+          self.AircraftOn = tk.Checkbutton(self.Labelframe1)
+          self.AircraftOn.place(x=100, y=30, height=20, width=100)
+          self.AircraftOn.configure(text="Aircraft")
+          self.AircraftOn.configure(anchor="w")
+          self.AircraftOn.configure(offvalue=False)
+          self.AircraftOn.configure(onvalue=True)
+          self.AircraftOn.configure(variable=self.AircraftOn_var)
+          self.AircraftOn.configure(command=lambda:MyTelegramBotGUI_support.Check_Press("AircraftOn"))
+
+          self.PolisenOn = tk.Checkbutton(self.Labelframe1)
+          self.PolisenOn.place(x=100, y=50, height=20, width=100)
+          self.PolisenOn.configure(text="Police")
+          self.PolisenOn.configure(anchor="w")
+          self.PolisenOn.configure(offvalue=False)
+          self.PolisenOn.configure(onvalue=True)
+          self.PolisenOn.configure(variable=self.PolisenOn_var)
+          self.PolisenOn.configure(command=lambda:MyTelegramBotGUI_support.Check_Press("PolisenOn"))
+          
+          self.DiceOn = tk.Checkbutton(self.Labelframe1)
+          self.DiceOn.place(x=200, y=10, height=20, width=100)
+          self.DiceOn.configure(text="Dice")
+          self.DiceOn.configure(anchor="w")
+          self.DiceOn.configure(offvalue=False)
+          self.DiceOn.configure(onvalue=True)
+          self.DiceOn.configure(variable=self.DiceOn_var)
+          self.DiceOn.configure(command=lambda:MyTelegramBotGUI_support.Check_Press("DiceOn"))
+
+          self.DefineOn = tk.Checkbutton(self.Labelframe1)
+          self.DefineOn.place(x=200, y=30, height=20, width=100)
+          self.DefineOn.configure(text="Define Word")
+          self.DefineOn.configure(anchor="w")       
+          self.DefineOn.configure(offvalue=False)
+          self.DefineOn.configure(onvalue=True)
+          self.DefineOn.configure(variable=self.DefineOn_var)
+          self.DefineOn.configure(command=lambda:MyTelegramBotGUI_support.Check_Press("DefineOn"))
+
+          self.PolisenOn = tk.Checkbutton(self.Labelframe1)
+          self.PolisenOn.place(x=200, y=50, height=20, width=100)
+          self.PolisenOn.configure(text="Map")
+          self.PolisenOn.configure(anchor="w")
+          self.PolisenOn.configure(offvalue=False)
+          self.PolisenOn.configure(onvalue=True)
+          self.PolisenOn.configure(variable=self.MapOn_var)
+          self.PolisenOn.configure(command=lambda:MyTelegramBotGUI_support.Check_Press("MapOn"))
+          
+          self.WebcamOn = tk.Checkbutton(self.Labelframe1)
+          self.WebcamOn.place(x=300, y=10, height=20, width=100)
+          self.WebcamOn.configure(text="Webcam")
+          self.WebcamOn.configure(anchor="w")
+          self.WebcamOn.configure(offvalue=False)
+          self.WebcamOn.configure(onvalue=True)
+          self.WebcamOn.configure(variable=self.WebcamOn_var)
+          self.WebcamOn.configure(command=lambda:MyTelegramBotGUI_support.Check_Press("WebcamOn"))
+          
+          self.Bot_Btn = tk.Button(self.top)
+          self.Bot_Btn.place(x=440, y=10, height=25, width=100)
+          self.Bot_Btn.configure(activebackground="#d9d9d9")
+          self.Bot_Btn.configure(activeforeground="black")
+          self.Bot_Btn.configure(background="gray")
+          self.Bot_Btn.configure(disabledforeground="#a3a3a3")
+          self.Bot_Btn.configure(font="-family {Segoe UI} -size 9")
+          self.Bot_Btn.configure(foreground="#000000")
+          self.Bot_Btn.configure(highlightbackground="#d9d9d9")
+          self.Bot_Btn.configure(highlightcolor="#000000")
+          self.Bot_Btn.configure(text="Run Bot")
+          self.Bot_Btn.bind('<Button-1>',lambda e:MyTelegramBotGUI_support.Button_Press(e,"Bot_Btn"))
+
+          self.SOS_ON_Btn = tk.Button(self.top)
+          self.SOS_ON_Btn.place(x=440, y=40, height=25, width=100)
+          self.SOS_ON_Btn.configure(activebackground="#d9d9d9")
+          self.SOS_ON_Btn.configure(activeforeground="black")
+          self.SOS_ON_Btn.configure(background="gray")
+          self.SOS_ON_Btn.configure(disabledforeground="#a3a3a3")
+          self.SOS_ON_Btn.configure(font="-family {Segoe UI} -size 9")
+          self.SOS_ON_Btn.configure(foreground="#000000")
+          self.SOS_ON_Btn.configure(highlightbackground="#d9d9d9")
+          self.SOS_ON_Btn.configure(highlightcolor="#000000")
+          self.SOS_ON_Btn.configure(text="Send SOS Msgs")
+          self.SOS_ON_Btn.bind('<Button-1>',lambda e:MyTelegramBotGUI_support.Button_Press(e,"SOS_ON_Btn"))
+
+          self.ScrollStatus = scrolledtext.ScrolledText(self.top)
+          self.ScrollStatus.place(x=5, y=105, height=app_height-130, width=540)
+          self.ScrollStatus.configure(background = 'light gray')
+          self.ScrollStatus.configure(state ='disabled')
+          
+          self.OutputLable = tk.Label(self.top)
+          self.OutputLable.place(x=5, y=app_height-20, height=20, width=345)
+          self.OutputLable.configure(text="Waiting ...")
+          self.OutputLable.configure(anchor="w")
+
+#init app
+def start_up():
+    MyTelegramBotGUI_support.main()
+
+if __name__ == '__main__':
+    MyTelegramBotGUI_support.main()
---
+++ MyTelegramBotGUI_support.py
@@ -0,0 +1,239 @@
+#! /usr/bin/env python3
+#  -*- coding: utf-8 -*-
+#
+# Support code for MyTelegramBot
+# version started 2024-05-10
+
+import tkinter as tk
+from tkinter.constants import *
+from tkinter.messagebox import *
+import MyTelegramBotGUI
+import time
+import threading
+import json
+import socket
+import logging
+
+################################### init App ##########################################
+def init_app():
+    global root
+    global App_flags, Client_Connected
+    global e
+    global _top1, _w1
+
+    Cient_Connected = False
+    e = threading.Event()
+    x = """{"Msg_type":"status_req", "Bot_Btn":"none", "SOS_ON_Btn":"none", "WeatherOn":"none", "HolidaysOn":"none",
+                    "DateTimeOn":"none", "AircraftOn":"none", "DiceOn":"none",
+                    "WebcamOn":"none", "DefineOn":"none", "TrafficOn":"none", "PoliceOn":"none", 
+                    "MapOn":"none","Alarm_Sleep":"0"}"""
+    App_flags = json.loads(x)
+
+    root = tk.Tk()
+    root.protocol( 'WM_DELETE_WINDOW' , root.destroy)
+    
+    # Creates a toplevel widget.
+    _top1 = root
+    _w1 = MyTelegramBotGUI.Toplevel1(_top1)
+    init(_top1, _w1)
+
+def init(top, gui, *args, **kwargs):
+    global w, top_level, root
+    w = gui
+    top_level = top
+    root = top
+
+############################### General Functions ####################################
+def Update_flags(flag: str):
+    obj = w.OutputLable
+    flag_status=App_flags[flag]
+    try:
+        eval(flag_status)
+        if eval(flag_status):
+            x = {flag :">False"}
+            App_flags.update(x)
+            App_flags.update({"Msg_type":"client_req"})
+            e.set()
+            obj.configure(text=flag + " stop signal sent ...")
+        else:
+            x = {flag:">True"}
+            App_flags.update(x)
+            App_flags.update({"Msg_type":"client_req"})
+            e.set()
+            obj.configure(text=flag + " start signal sent ...")
+    except:
+        return
+
+def Update_check_flags(flag: str, ck_state: int):
+    obj = w.OutputLable
+    if ck_state:
+        x = {flag:">True"}
+        App_flags.update(x)
+        App_flags.update({"Msg_type":"client_req"})
+        e.set()
+        obj.configure(text=flag + " start signal sent ...")
+    elif not ck_state:
+        x = {flag :">False"}
+        App_flags.update(x)
+        App_flags.update({"Msg_type":"client_req"})
+        e.set()
+        obj.configure(text=flag + " stop signal sent ...")
+
+################################ Menu handlers ########################################
+def clear_messages():
+    obj = w.ScrollStatus
+    obj.configure(state ='normal')
+    obj.delete('1.0',END)
+    obj.configure(state ='disabled')
+    return
+
+def close_app():
+    root.destroy()
+
+def about_msg():
+    about_txt= "This is a control app for the MyTelegramBot.  It is a\n"
+    about_txt+="application written by LateForTrain in 2024.  The main funcion\n"
+    about_txt+="is to enable controll of which functions are available.\n"
+    print(showinfo("About TBC", about_txt))
+
+############################### Button handlers #######################################
+def Button_Press(*args):
+    App_flags.update({"Msg_type":"client_req"})
+    Update_flags(args[1])
+
+def Check_Press(*args):
+    evaltext="w."+args[0]+"_var"
+    obj=eval(evaltext)
+    ck_state=obj.get()
+    Update_check_flags(args[0],ck_state)
+
+################################### Thread ##########################################
+def Update_Button(element: str):
+    obj1 = w.OutputLable
+    eval_text = "w." + element
+    try:
+        obj = eval(eval_text)
+        if App_flags[element] == "True":
+            obj.configure(background="Green")
+            obj1.configure(text=element + " start set.")
+        elif App_flags[element] == "False":
+            obj.configure(background="Red")
+            obj1.configure(text=element + " stop set.")
+    except:
+        return
+
+def Update_Check(element: str):
+    obj1 = w.OutputLable
+    eval_text = "w." + element+"_var"
+    try:
+        obj = eval(eval_text)
+        if App_flags[element] == "True":
+            obj.set(True)
+            obj1.configure(text=element + " start set.")
+        elif App_flags[element] == "False":
+            obj.set(False)
+            obj1.configure(text=element + " stop set.")
+    except:
+        return
+        
+def client(e):
+    logger.info("GUI: Started client thread")
+    global App_flags, App_Connected
+    host = "127.0.0.1"  # as both code is running on same pc
+    port = 5000  # socket server port number
+
+    while True:
+        try:
+            if not Client_Connected:
+                client_socket = socket.socket()  # instantiate
+                client_socket.connect((host, port))  # connect to the server
+                Client_Connected = True
+                logger.info("GUI: Client connected")
+            event_is_set = e.wait()
+            e.clear()
+            msg = json.dumps(App_flags)
+            logger.debug('GUI: Send to server: ' + msg)
+            client_socket.send(msg.encode())  # send message
+            data = client_socket.recv(1024).decode()  # receive response
+            logger.debug('Gui: Received from server: ' + data)
+            App_flags_new=json.loads(data)
+            for element in App_flags_new:
+                if App_flags[element] != App_flags_new[element]:
+                    App_flags[element] = App_flags_new[element]
+                    if element.find("Btn") >= 0:
+                        Update_Button(element)
+                    else:
+                        Update_Check(element)
+            e.clear()
+            logger.debug('GUI: After Processing: ' + json.dumps(App_flags))
+        except:
+            logger.warning("GUI: failed connect try")
+            Client_Connected = False
+            time.sleep(1)
+
+    client_socket.close()  # close the connection
+
+def server():
+    logger.info("GUI: Started server thread")
+    obj = w.ScrollStatus
+    # get the hostname
+    host = "127.0.0.1"
+    port = 5001  # initiate port no above 1024
+
+    server_socket = socket.socket()  # get instance
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+    logger.info("GUI: Socket binded to %s" %(port))
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    while True:
+        conn, address = server_socket.accept()  # accept new connection
+        logger.info("GUI: Connection from: " + str(address))
+        connected_flag = True
+        while connected_flag:
+            # receive data stream. it won't accept data packet greater than 1024 bytes
+            try:
+                data = conn.recv(1024).decode()
+            except:
+                connected_flag = False
+                break
+            if not data:
+                # if data is not received break
+                break
+            obj.configure(state ='normal')
+            obj.insert(INSERT,data+"\n")
+            obj.insert(INSERT,"--------------------------------------------------------------"+"\n")
+            obj.configure(state ='disabled')
+            obj.see(END)
+    conn.close()  # close the connection
+
+def init_threads():
+    MyClient = threading.Thread(target=client,args=(e,))
+    MyClient.daemon = True
+    MyClient.start()
+    
+    MyServer = threading.Thread(target=server)
+    MyServer.daemon = True
+    MyServer.start()
+
+############################### Main definition #####################################
+def main(*args):
+    global logger
+
+    # Enable logging for telegram bot
+    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+    # set higher logging level for httpx to avoid all GET and POST requests being logged
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    logger = logging.getLogger(__name__)
+
+    init_app()
+    init_threads()
+    time.sleep(1)
+    e.set()
+    logger.info("GUI: Main loop started...")
+    root.mainloop()
+
+#executer when class is created
+if __name__ == '__main__':
+    MyTelegramBotGUI.start_up()
